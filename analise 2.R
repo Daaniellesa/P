19fdc5edf8b89f04358b91c04399b9348af61d4d
Danielle <222007101@aluno.unb.br>
2023-10-20 03:19:38 -0300

    .

--- analise 2.R
+++ analise 2.R
@@ -13,3 +13,17 @@ ggplot(limpos) +
   labs(x = "Marca", y = "Preço") +
   theme_estat()
 ggsave("box_bi.pdf", width = 158, height = 93, units = "mm")
+
+quadro_resumo <- limpos %>% 
+  group_by(Marca) %>% 
+  summarize(Média = round(mean(Preço),2),
+            `Desvio Padrão` = round(sd(Preço),2),
+            `Variância` = round(var(Preço),2),
+            `Mínimo` = round(min(Preço),2),
+            `1º Quartil` = round(quantile(Preço, probs = .25),2),
+            Mediana = round(quantile(Preço, probs = .5),2),
+            `3º Quartil` = round(quantile(Preço, probs = .75),2),
+            `Máximo` = round(max(Preço),2)) %>% t() %>% as.data.frame() %>% 
+  mutate(V1 = str_replace(V1,"\\.",","))
+quadro_resumo  
+
